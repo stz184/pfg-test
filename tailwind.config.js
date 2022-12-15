f8ef5e811ad2aec8abb8a5d74a22ecf179df2df3
Vladimir Ivanov <info@vladimir-ivanov.net>
2022-12-15 22:55:07 +0200

    Added basic layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{ts,tsx,css}",
+    "./src/**/*.{ts,tsx,css}", "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
 }
