e9ccb72d575bf36fd877bc2713d6adc6867a817a
Vladimir Ivanov <info@vladimir-ivanov.net>
2022-12-24 00:46:07 +0200

    - Added search functionality

--- src/components/NewsGrid.tsx
+++ src/components/NewsGrid.tsx
@@ -1,13 +1,26 @@
-import {useContext, useEffect, useState} from "react";
+import {useCallback, useContext, useEffect, useState} from "react";
 import {UserContext} from "../App";
 import NewsService, {Article} from "../services/NewsService";
 import ArticleCard from "./ArticleCard";
 import LoadingSpinner from "./LoadingSpinner";
+import {useSearchParams, Link} from "react-router-dom";
+// @ts-ignore
+import escape from "regexp.escape";
+
 
 const NewsGrid = () => {
-    const { isLoggedIn } = useContext(UserContext);
-    const [ articles, setArticles ] = useState([] as Article[]);
-    const [ isLoading, setIsLoading ] = useState(true);
+    const {isLoggedIn} = useContext(UserContext);
+    const [articles, setArticles] = useState([] as Article[]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [search] = useSearchParams();
+
+    const searchNews = useCallback((article: Article) => {
+        const query = search.get('q');
+        if (!query) return true;
+
+        const rx = new RegExp(escape(query), 'ig');
+        return article.title.search(rx) > -1 || article.content.search(rx) > 1;
+    }, [search]);
 
     useEffect(() => {
         setIsLoading(true);
@@ -18,11 +31,24 @@ const NewsGrid = () => {
 
     if (!isLoggedIn || (articles.length === 0 && !isLoading)) return <></>;
 
-    if (isLoading) return <LoadingSpinner />;
+    if (isLoading) return <LoadingSpinner/>;
+
+    const articlesFiltered = articles.filter((article: Article) => searchNews(article));
 
     return (
-        <div className="flex flex-wrap justify-around float-left">
-            {articles.map((article) => <ArticleCard key={article.id} article={article} />)}
+        <div className="flex flex-wrap justify-around">
+            {articlesFiltered
+                .filter((article: Article) => searchNews(article))
+                .map((article) => <ArticleCard key={article.id} article={article}/>)
+            }
+            {articles.length > 0 && articlesFiltered.length == 0 && (
+                <div className="text-center text-gray-800 pb-6 ml-auto mr-auto">
+                    <h1 className="text-5xl font-bold mt-0 mb-6">No results found</h1>
+                    <h3 className="text-3xl font-bold mb-8">Try searching with different words</h3>
+                    <Link
+                        className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                        data-mdb-ripple="true" data-mdb-ripple-color="light" to='/' role="button">Go back</Link>
+                </div>)}
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {createBrowserRouter, Router} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import NewsGrid from "./components/NewsGrid";
 import Article from "./components/Article";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <NewsGrid />,
-    },
-    {
-        path: "news/:articleId",
-        element: <Article />,
-    },
-]);
+const router = createBrowserRouter([{
+    element: <App/>,
+    children: [
+        {
+            path: "/",
+            element: <NewsGrid/>,
+        },
+        {
+            path: "news/:articleId",
+            element: <Article/>,
+        }
+    ]
+}]);
 
 root.render(
-  <React.StrictMode>
-    <App router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
